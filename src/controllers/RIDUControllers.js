c89b84cb3b99ce2bad3b337da8a96040699fdd40
Anikesh Patel <anikesh.consultadd@gmail.com>
2020-04-11 11:06:34 +0530

    Deployment Commit

--- src/controllers/RIDUControllers.js
+++ src/controllers/RIDUControllers.js
@@ -19,3 +19,28 @@ module.exports.insertMany = (db, col, data) => {
     const collection = hyperlite.getDB(db).collection(col)
     return collection.insertMany(data)
 }
+
+module.exports.delete = (db, col, query) => {
+    const collection = hyperlite.getDB(db).collection(col)
+    return collection.delete(query)
+}
+
+module.exports.deleteMany = (db, col, query) => {
+    const collection = hyperlite.getDB(db).collection(col)
+    return collection.deleteMany(query)
+}
+
+module.exports.update = (db, col, query, data) => {
+    const collection = hyperlite.getDB(db).collection(col)
+    return collection.update(query, data)
+}
+
+module.exports.updateMany = (db, col, query, data) => {
+    const collection = hyperlite.getDB(db).collection(col)
+    return collection.updateMany(query, data)
+}
+
+module.exports.pipeline = (db, col, pipeline) => {
+    const collection = hyperlite.getDB(db).collection(col)
+    return collection.aggregate(pipeline)
+}
